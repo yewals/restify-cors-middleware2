cabac5e61207c525946e6b441b92315366f25e77
Jim Patterson <jpatterson@datatrac.com>
2019-09-04 14:39:16 -0400

    Update dependencies & code style changes to match

--- src/actual.js
+++ src/actual.js
@@ -4,11 +4,11 @@ var constants = require('./constants.js')
 exports.handler = function (options) {
   var matcher = originMatcher.create(options.origins)
   return function (req, res, next) {
-    var originHeader = req.headers['origin']
+    var originHeader = req.headers.origin
 
     // Since we use the origin header to control what we return, that
     // means we vary on origin
-    res.setHeader(constants['STR_VARY'], constants['STR_VARY_DETAILS'])
+    res.setHeader(constants.STR_VARY, constants.STR_VARY_DETAILS)
 
     // If either no origin was set, or the origin isn't supported, continue
     // without setting any additional headers
@@ -17,11 +17,11 @@ exports.handler = function (options) {
     }
 
     // if match was found, let's set some headers.
-    res.setHeader(constants['AC_ALLOW_ORIGIN'], originHeader)
+    res.setHeader(constants.AC_ALLOW_ORIGIN, originHeader)
     if (options.credentials) {
-      res.setHeader(constants['AC_ALLOW_CREDS'], 'true')
+      res.setHeader(constants.AC_ALLOW_CREDS, 'true')
     }
-    res.setHeader(constants['AC_EXPOSE_HEADERS'],
+    res.setHeader(constants.AC_EXPOSE_HEADERS,
       options.exposeHeaders.join(', '))
 
     return next()
